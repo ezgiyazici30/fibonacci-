804ef597f79d32d7c959fc191c053e8caa528284
ezgiyazici30 <ezgiyazici3011@gmail.com>
2024-11-21 14:48:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Bir sayı giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+
+        
+        int a = 0, b = 1;
+
+        Console.WriteLine("Fibonacci Serisi:");
+
+        
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(a + " ");
+
+            
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
